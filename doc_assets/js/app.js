340632d7f28a707cd6ef4e84e798d4140e09b839
Simon Wallström <simon.wallstrom@yahoo.se>
2015-01-21 17:05:11 +0100

    added some new modules

--- doc_assets/js/app.js
+++ doc_assets/js/app.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
+
+    // Added active class to header nav
     var url = window.location.href;
 
-    console.log(url);
-
-    // Will also work for relative and absolute hrefs
     $('.site-nav a').filter(function() {
         return this.href == url;
     }).parent().addClass('active');
+
+
+    // Toggle modal
+    $(".modal-trigger").on("click", function() {
+        $(".modal").addClass("modal-open");
+        $("body").addClass("modal-overlay");
+    });
 });
